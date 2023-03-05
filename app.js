63175d990527310f092dc5bfd1a0a78a9a9fb0bc
iabdamit <101884066+iabdamit@users.noreply.github.com>
2023-03-05 14:12:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ const reviewRoutes = require('./routes/reviews');
 const MongoDBStore = require("connect-mongo")(session);
 
 //const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp';
-mongoose.set('strictQuery', true);
+const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+mongoose.set('strictQuery',false);
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
